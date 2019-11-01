a9b12f6caa3cbef70f3ecbcbba0285dd4637384e
yinyuWu <2936932419@qq.com>
2019-11-02 07:50:40 +1100

    add json file

--- recoAlgorithm/reco.py
+++ recoAlgorithm/reco.py
@@ -8,6 +8,13 @@ def get_data_from_api():
     # get info from api and convert it to a json 
     r = requests.get('https://api.edamam.com/search?app_id=97875047&app_key=13a31b794de48e8c01b66e91ef648500&q=Special&diet=balanced&from=0&to=100')
     json_text = r.json()
+    with open("recipes.json", 'w', encoding= 'utf-8') as f:
+        json.dump(json_text, f, ensure_ascii=False, indent=4)
+
+
+def parse_data_to_dataframe():
+    with open('recipes.json', 'r') as f:
+        json_text = json.load(f)
 
     # extract recipes from json
     hits = json_text['hits']
@@ -21,4 +28,5 @@ def get_data_from_api():
     return df
 
 if __name__ == "__main__":
-    get_data_from_api()
+    #get_data_from_api()
+    parse_data_to_dataframe()
