1c44aa2a381e76940e00f718a7ac9e4941447fc8
yinyuWu <2936932419@qq.com>
2019-11-02 01:18:58 +1100

    get info from api

--- recoAlgorithm/reco.py
+++ recoAlgorithm/reco.py
@@ -3,15 +3,22 @@ import pandas as pd
 import json
 import requests
 
-r = requests.get('https://api.edamam.com/search?app_id=97875047&app_key=13a31b794de48e8c01b66e91ef648500&q=Special&diet=balanced&from=0&to=5')
-x = r.json()
-#df = pd.DataFrame(x['hits'][0]['recipe'])
-h = x['hits']
-#print(h[0]['recipe'])
 
-dic_list = []
-for i in range(5):
-    dic_list.append(h[i]['recipe'])
+def get_data_from_api():
+    # get info from api and convert it to a json 
+    r = requests.get('https://api.edamam.com/search?app_id=97875047&app_key=13a31b794de48e8c01b66e91ef648500&q=Special&diet=balanced&from=0&to=100')
+    json_text = r.json()
 
-df = pd.DataFrame(dic_list)
-print(df)
+    # extract recipes from json
+    hits = json_text['hits']
+    dic_list = []
+    for i in range(len(hits)):
+        dic_list.append(hits[i]['recipe'])
+
+    # convert recipes to dataframe
+    df = pd.DataFrame(dic_list)
+    print(df)
+    return df
+
+if __name__ == "__main__":
+    get_data_from_api()
