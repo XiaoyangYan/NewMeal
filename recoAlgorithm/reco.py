f788ad896ffce65383ada89ce891938b93d11ec6
yinyuWu <2936932419@qq.com>
2019-11-02 10:51:15 +1100

    add knn

--- recoAlgorithm/reco.py
+++ recoAlgorithm/reco.py
@@ -2,7 +2,9 @@ import numpy as np
 import pandas as pd
 import json
 import requests
+import torch
 
+from sklearn.neighbors import NearestNeighbors
 
 def get_data_from_api():
     # get info from api and convert it to a json 
@@ -24,9 +26,31 @@ def parse_data_to_dataframe():
 
     # convert recipes to dataframe
     df = pd.DataFrame(dic_list)
-    print(df)
+    df = df.drop(['uri','url','yield', 'source', 'shareAs', 'ingredients', 
+    'totalNutrients', 'digest', 'image', 'totalWeight', 'totalDaily'], axis=1)
     return df
+
+def split_data(df_data):
+    data = df_data.to_numpy()
+    size = data.shape[0]
+    train_size = (int)(size * 0.7)
+    train = data[:train_size]
+    test = data[train_size:]
+    print(train.shape)
+    print(test.shape)
+
+def knn(X):
+    nbrs = NearestNeighbors(n_neighbors=10, algorithm='ball_tree').fit(X)
+
+def dl():
+    pass
+
+
 
 if __name__ == "__main__":
     #get_data_from_api()
-    parse_data_to_dataframe()
+    df = parse_data_to_dataframe()
+    print(df.columns)
+    #split_data(df)
+
+    
