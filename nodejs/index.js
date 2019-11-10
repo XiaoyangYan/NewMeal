4f961b6fdd0399411c4cfb16ab7a3c12426ec93c
XiaoyangYan <z5167310@student.unsw.edu.au>
2019-11-11 00:35:43 +1100

    add create recipe page

--- nodejs/index.js
+++ nodejs/index.js
@@ -2,6 +2,13 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 app.use(bodyParser.json());
+app.use(function(req, res, next) {
+        res.header("Access-Control-Allow-Origin", "*");
+        res.header("Access-Control-Allow-Headers", "Content-Type,Content-Length, Authorization, Accept,X-Requested-With");
+        res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
+        res.header("X-Powered-By",' 3.2.1')
+        next();
+      });
 require("dotenv").config();
 const recipeRouter = require("./router/recipe");
 
